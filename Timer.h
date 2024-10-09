a1bd1f916ab24b366d751f3ca7229672c486b11f
keno555552 <kwankinleung5@gmail.com>
2024-10-09 14:52:44 +0900

    Timer　3つ追加

--- Timer.h
+++ Timer.h
@@ -1,4 +1,5 @@
-#pragma once
+﻿#pragma once
+#include"texture.h"
 
 /// <summary>
 /// {clock,time}
@@ -7,3 +8,28 @@ struct Timer final {
 	float clock;
 	float time;
 };
+
+
+
+
+/// <summary>
+/// 動画運行のタイマー
+/// </summary>
+/// <param name="Timer">動画タイマーの関数</param>
+void TimerRunAnimtion(Timer* Timer);
+
+
+
+/// <summary>
+/// 動画フレームの転換
+/// </summary>
+/// <param name="Timer">動画タイマーの関数</param>
+/// <returns></returns>
+int TimerToFrame(const Timer Timer);
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Timer"></param>
+void TimerRunZero(Timer* Timer);
