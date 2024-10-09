e82e31772750752e1537dbcf83c338d90908cc7b
悠太 見原 <yuuta_mippa@icloud.com>
2024-10-09 17:10:35 +0900

    敵の追尾処理更新

--- enemy.h
+++ enemy.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Vector2.h>
+#include "obj.h"
 
 enum EnemyDirection {
 	UP = 0,
@@ -11,6 +12,9 @@ enum EnemyDirection {
 struct Enemy {
 	Vector2 pos;
 	Vector2 velocity;
+	Vector2 components;
+	Vector2 directions;
+	float magnitude;
 	float width;
 	float height;
 	int moveTimer;
@@ -27,6 +31,8 @@ void InitEnemyNormal(Enemy& enemy);
 void InitEnemyHorming(Enemy& enemy);
 
 void EnemyMove(Enemy& enemy);
-void EnemyMoveHorming(Enemy& enemy);
+void EnemyMoveHorming(Enemy& enemy, Obj& player);
 
 void RenderEnemy(Enemy& enemy, Vector2& scroll);
+
+void EnemyDebug(Enemy& enemy);
