822ec5fe7053fd2ed096b5589bec2fb213b2a241
悠太 見原 <yuuta_mippa@icloud.com>
2024-10-10 09:54:23 +0900

    デバッグ用の処理を削除

--- main.cpp
+++ main.cpp
@@ -92,7 +92,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		RenderEnemy(enemy, scroll);
 		RenderEnemy(hormingEnemy, scroll);
-		EnemyDebug(hormingEnemy);
 		Novice::ScreenPrintf(0, 0, "scroll x : %.2f y : %.2f", scroll.x, scroll.y);
 
 		viewDig(&system.digFlat, keys[DIK_P], preKeys[DIK_P], keys[DIK_LBRACKET], preKeys[DIK_LBRACKET], keys[DIK_RBRACKET], preKeys[DIK_RBRACKET]);
